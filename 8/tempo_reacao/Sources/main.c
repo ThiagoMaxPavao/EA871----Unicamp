d823cf3de6c7b4fe69c4b2e1b1d10c3d73afe4fc
vinicius <vinemprego@gmail.com>
2023-05-19 10:34:25 -0300

    configura tpm

--- 8/tempo_reacao/Sources/main.c
+++ 8/tempo_reacao/Sources/main.c
@@ -17,5 +17,11 @@ int main(void)
 	GPIO_initLCD();
 	SIM_setaTPMSRC(1);
 	SIM_setaFLLPLL(0);
+	
+	TPM_config_especifica(0, 40960, 0b1111, 0, 0, 0, 0, 0, 0, 0b111);
+	TPM_CH_config_especifica();
+	
+	TPM_config_especifica(1, 1500, 0b1111, 0, 0, 0, 0, 0, 0, 0b101);
+	TPM_CH_config_especifica();
 	return 0;
 }
