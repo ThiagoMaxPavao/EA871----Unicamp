47dea05b7aeec3ea57e0b00fdaecb08d2e79ae0c
Thiago Pavao <thipavao44@gmail.com>
2023-06-06 20:21:11 -0300

    limpa posicoes dos numeros

--- 9/controle_cooler/Sources/main.c
+++ 9/controle_cooler/Sources/main.c
@@ -105,9 +105,12 @@ int main(void)
 			}
 			
 			// LCD
+			GPIO_escreveStringLCD(0x07, (uint8_t*) "     "); // limpa posicoes dos numeros
+			GPIO_escreveStringLCD(0x47, (uint8_t*) "     ");
 			ftoa(duty, buffer, 2);
 			GPIO_escreveStringLCD(0x07, (uint8_t*) buffer); // atualiza duty
 			ftoa(temp, buffer, 2);
+			GPIO_escreveStringLCD(0x07, (uint8_t*) buffer);
 			GPIO_escreveStringLCD(0x47, (uint8_t*) buffer); // atualiza temperatura
 			
 			// Cooler
