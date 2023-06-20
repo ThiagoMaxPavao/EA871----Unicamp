5967a292e12db1c9e4a95ea49c7e993ba0c74b9b
Thiago Pavao <thipavao44@gmail.com>
2023-06-20 18:01:38 -0300

    funcoes auxiliares para o LED

---
+++ ProjetoSimon/teste-LED/Project_Headers/LED_Matrix.h
@@ -0,0 +1,74 @@
+/*
+ * @file LED_Matrix.h
+ * @brief Declaracao das funcoes de auxilio no uso da matriz de LEDs
+ * @author Thiago Pavao
+ * @author Vinicius Mantovani
+ * @date 20/06/2023
+ */
+
+#ifndef LED_MATRIX_H_
+#define LED_MATRIX_H_
+//Inclusao dos tipos de dados padronizados pelo ISO C99
+#include "stdint.h"
+
+/** 
+ * @brief inicializa os pinos do PORTE que serao utilizados na comunicacao com a matriz de LEDs
+ * PTE1 - SPI1_MOSI, PTE2 - SPI1_SCK, PTE4 - SPI1_PCS0
+ */
+void LEDM_init_pins();
+
+/** 
+ * @brief inicializa o modulo SPI, para comunicacao com MAX7219, da matriz de LEDs
+ */
+void LEDM_init_SPI();
+
+/** 
+ * @brief Envia um dado para ser escrito em um dos registradores de MAX7219. Dois bytes sao enviados na comunicacao
+ * @param[in] addrs endereco do registrador que se deseja escrever o dado
+ * @param[in] dado a ser escrito no registrador
+ */
+void LEDM_escreve(uint8_t addrs, uint8_t dado);
+
+/** 
+ * @brief Inicializa a matriz de LEDs, configurando tipo de decodificacao, brilho e etc. Isto so pode ser feito apos a incializacao de SPI
+ * @param[in] brilho dos leds, valor entre 0x0 e 0xf (inclusive)
+ */
+void LEDM_init_matrix(uint8_t brilho);
+
+/** 
+ * @brief Limpa a tela da matriz, apagando todos os LEDs.
+ */
+void LEDM_clear();
+
+/** 
+ * @brief Escreve um caractere na matriz de LEDs
+ * @param[in] caractere a ser escrito. Aceita: 0-9, a-z, A-Z (mas sempre imprime a letra maiuscula)
+ */
+void LEDM_escreve_char(char caractere);
+
+/** 
+ * @brief Escreve uma string na matriz de LEDs, piscando os caracteres na ordem com um delay entre eles
+ * @param[in] string a ser escrita
+ * @param[in] delay entre as letras, em multiplos de 5us
+ */
+void LEDM_escreve_string(char* string, int delay);
+
+/** 
+ * @brief Desenha a grade do jogo, mostrando as separacoes entre as casas
+ */
+void LEDM_desenha_grade();
+
+/** 
+ * @brief Acende uma posicao na tela, especificada por um numero de 1 a 9. A grade tambem eh acessa
+ * @param[in] posicao que deve ser acendida
+ */
+void LEDM_acende_posicao(uint8_t posicao);
+
+/** 
+ * @brief Acende varias posicoes, baseado no vetor enviado. A grade nao eh acessa junto, apenas as posicoes sao mostradas.
+ * @param[in] posicoes vetor de posicoes que devem ser acessas
+ * @param[in] numero de posicoes no vetor
+ */
+void LEDM_acende_posicoes(uint8_t* posicoes, uint8_t n);
+
+#endif /* LED_MATRIX_H_ */
---
+++ ProjetoSimon/teste-LED/Sources/LED_Matrix.c
@@ -0,0 +1,120 @@
+/*
+ * @file LED_Matrix.c
+ * @brief Este arquivo contem as funcoes necessarias para comunicacao com a matriz de LEDs
+ * @author Thiago Pavao
+ * @author Vinicius Mantovani
+ * @date 20/06/2023
+ */
+
+#include "derivative.h"
+#include "util.h"
+#include "font.h"
+
+
+void LEDM_init_pins() {
+	// Habilita o clock do modulo PORTE
+	SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK ;    		
+
+	PORTE_PCR1 |= PORT_PCR_MUX(0x2);  
+
+	PORTE_PCR2 |= PORT_PCR_MUX(0x2);  
+
+	PORTE_PCR4 |= PORT_PCR_MUX(0x2);  
+
+}
+
+void LEDM_init_SPI(){
+
+	SIM_SCGC4 |= SIM_SCGC4_SPI1_MASK;
+	
+	SPI1_C1 |= SPI_C1_SPE_MASK;
+	
+	SPI1_C1 |= SPI_C1_MSTR_MASK;
+	
+	SPI1_C1 |= SPI_C1_CPOL_MASK;
+	
+	SPI1_C1 |= SPI_C1_SSOE_MASK;
+	
+	SPI1_C2 |= SPI_C2_MODFEN_MASK;
+	
+	SPI1_C2 |= SPI_C2_BIDIROE_MASK;
+	
+	SPI1_C2 |= SPI_C2_SPISWAI_MASK;
+
+	SPI1_BR |= SPI_BR_SPPR(0b111);
+	//SPI1_C1 |= SPI_C1_SPIE_MASK; //interrupt
+	
+	
+	//5. In the master, read SPIx_S while SPTEF = 1, and then write to the transmit data register (SPIx_D) to begin transfer
+
+	/*if(SPI1_S && SPI_S_SPTEF_MASK ){
+		SPI1_D |= SPI_D_Bits(0b10101010);
+	}*/
+}
+
+void LEDM_escreve(uint8_t addrs, uint8_t dado){
+	while(!(SPI1_S && SPI_S_SPTEF_MASK));
+	SPI1_D |= SPI_D_Bits(addrs);
+	
+	while(!(SPI1_S && SPI_S_SPTEF_MASK));
+	SPI1_D |= SPI_D_Bits(dado);
+	
+	espera_5us(10); // garante processamento da transmissao
+}
+
+void LEDM_init_matrix(uint8_t brilho) {
+	LEDM_escreve(0x09, 0x00);	// No decoding
+	LEDM_escreve(0x0a, brilho);	// Brightness (max = 0xf)
+	LEDM_escreve(0x0b, 0x07);	// Scanlimit completo
+	LEDM_escreve(0x0c, 0x01);	// normal mode (not shutdown)
+	LEDM_escreve(0x0f, 0x00);	// normal mode (not display test)
+}
+
+void LEDM_clear() {
+	uint8_t i;
+	for(i = 1; i <= 8; i++)
+		LEDM_escreve(i, 0x00);
+}
+
+void LEDM_escreve_char(char caractere) {
+	int i;
+	for(i = 1; i <= 8; i++)
+		LEDM_escreve(i, font[(int)caractere][i-1]);
+}
+
+void LEDM_escreve_string(char* string, int delay) {
+	char c;
+	while((c = *(string++)) != 0) {
+		LEDM_escreve_char(c);
+		espera_5us(delay);
+	}
+}
+
+void LEDM_desenha_grade() {
+	int i;
+	for(i = 1; i <= 8; i++)
+		LEDM_escreve(i, i%3 == 0 ? 0xff : 0x24);
+}
+
+void LEDM_acende_posicao(uint8_t posicao) {
+	int i;
+	for(i = 1; i <= 8; i++) {
+		uint8_t byte = i%3 == 0 ? 0xff : 0x24; // base, grade
+		if(i/3 == (posicao-1)/3) { // se posicao estiver na linha atual
+			byte |= 0b11 << ((2-(posicao-1)%3) * 3); // posiciona 2 bits 1, dependendo do resto da divisao de (posicao-1) por tres
+		}
+		LEDM_escreve(i, byte);
+	}
+}
+
+void LEDM_acende_posicoes(uint8_t* posicoes, uint8_t n) {
+	int i, j;
+	for(i = 1; i <= 8; i++) {
+		uint8_t byte = 0;
+		for(j = 0; j < n; j++)
+			if(i%3 != 0 && i/3 == (posicoes[j]-1)/3) { // se posicao estiver na linha atual
+				byte |= 0b11 << ((2-(posicoes[j]-1)%3) * 3); // posiciona 2 bits 1, dependendo do resto da divisao de (posicao-1) por tres
+			}
+		LEDM_escreve(i, byte);
+	}
+}
