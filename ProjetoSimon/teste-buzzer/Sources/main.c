422c9158b0d7ef2e793c98a7b55614b907d7f178
Thiago <thipavao44@gmail.com>
2023-06-20 21:45:41 -0300

    funcoes do buzzer em arquivos especificos

--- ProjetoSimon/teste-buzzer/Sources/main.c
+++ ProjetoSimon/teste-buzzer/Sources/main.c
@@ -8,21 +8,7 @@
 #include "SIM.h"
 #include "TPM.h"
 #include "util.h"
-
-void liga_buzzer(uint8_t tone) {
-	int base = 500;
-	int range = 2000 - base;
-	float step = range / (9 - 1);
-	float f = base + (tone - 1) * step;
-	uint16_t mod = (uint16_t) (20971520/(32*f));
-	
-	TPM_config_especifica(2, mod, 0b1111, 0, 0, 0, 0, 0, 0b101);
-	TPM_CH_config_especifica(2, 0, 0b1001, mod/2);
-}
-
-void desliga_buzzer() {
-	TPM_CH_config_especifica(2, 0, 0b0000, 0);
-}
+#include "buzzer.h"
 
 int main(void)
 {
@@ -35,7 +21,7 @@ int main(void)
 
 	for(;;) {
 		liga_buzzer(a);
-		espera_5us(200000); // 1 segundo = 200000
+		espera_5us(200000 * .3); // 1 segundo = 200000
 		desliga_buzzer();
 		
 		a = (a == 9 ? 1 : a+1);
