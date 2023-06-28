d79cc2961dcbff4f9ec424bf98656a0af7aa9946
vinicius <vinemprego@gmail.com>
2023-06-28 10:14:37 -0300

    GENIUS

--- ProjetoSimon/GENIUS/Sources/ISR.c
+++ ProjetoSimon/GENIUS/Sources/ISR.c
@@ -18,37 +18,19 @@ static uint16_t CT1;
 static uint8_t seguranca;
 static BufferCirc_type buffer_leitura;
 
-/*
- * 0x2010 - POWER
- * 0x2022 - OK
- * 0x20e0 - Seta esquerda
- * 0x2060 - Seta direita
- * 0x2002 - Seta cima
- * 0x2082 - Seta baixo
- * 0x2088 - NUMPAD1
- * 0x2048 - NUMPAD2
- * 0x20c8 - NUMPAD3
- * 0x2028 - NUMPAD4
- * 0x20a8 - NUMPAD5
- * 0x2068 - NUMPAD6
- * 0x20e8 - NUMPAD7
- * 0x2018 - NUMPAD8
- * 0x2098 - NUMPAD9
- * 0x2008 - NUMPAD0
- */
-
-/*void FTM1_IRQHandler () {
+void FTM1_IRQHandler () {
 	
 	if (TPM1_STATUS & TPM_STATUS_TOF_MASK) {
 			
-		if(estado == LEITURA) {
+		if(estado == LEITURA_INICIO || estado == LEITURA_MAPA || estado == LEITURA_JOGO) {
 			if(seguranca) { // indica que nao houveram pulsos em um tempo longo demais, maior do que o maior esperado (9ms),
 							// pois houveram dois overflows antes de qualquer borda no sinal (de subida ou descida).
 				BC_push (&buffer_leitura, 0xffff);
 			} else {
 				seguranca = 1;
 			}
-		} else {
+		}		
+		else {
 			TPM1_SC &= ~TPM_SC_TOIE_MASK; // desabilitar a interrupcao por overflow
 		}
 		
@@ -56,17 +38,29 @@ static BufferCirc_type buffer_leitura;
 	}
 	
 	if (TPM1_STATUS & TPM_STATUS_CH0F_MASK) {
-		if(estado == ESPERA) {
+		if(estado == INICIO || estado == ESPERA_MAPA || estado == ESPERA_JOGO) {
 			CT1 = TPM1_C0V;
 			BC_free (&buffer_leitura);
 			seguranca = 0;
 			// habilita overflow para detectar corte no sinal
 			TPM1_SC |= (TPM_SC_TOF_MASK |	// resetar flag
 						TPM_SC_TOIE_MASK);	// habilitar a interrupcao TOF
-			estado = LEITURA;
+			switch(estado){
+			case INICIO:
+				estado = LEITURA_INICIO;
+				break;
+			case ESPERA_MAPA:
+				estado = LEITURA_MAPA;
+				break;
+			case ESPERA_JOGO:
+				estado = LEITURA_JOGO;
+				break;
+			default:
+				break;
+			}
 		}
 		
-		else if(estado == LEITURA) {
+		else if(estado == LEITURA_INICIO || estado == LEITURA_MAPA || estado == LEITURA_JOGO) {
 			uint16_t CT2 = TPM1_C0V;
 			
 			if(CT2 >= CT1)	BC_push (&buffer_leitura, CT2 - CT1);
@@ -78,7 +72,7 @@ static BufferCirc_type buffer_leitura;
 		
 		TPM1_C0SC |= TPM_CnSC_CHF_MASK;
 	}
-}*/
+}
 
 tipo_estado ISR_LeEstado() {
 	return estado;
